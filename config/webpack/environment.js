981cc24313f67a51f675a1a21d1803795dbbd68c
小林　良輝 <yoshikikobayashi@yoshikiMacBook-Pro.local>
2020-07-15 14:44:25 +0900

    Practice Docker

---
+++ config/webpack/environment.js
@@ -0,0 +1,19 @@
+const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+environment.config.resolve.alias = {'vue$': 'vue/dist/vue.esm.js' }
+// jQueryとBootstapのJSを使えるように
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
+)
+
+module.exports = environment
