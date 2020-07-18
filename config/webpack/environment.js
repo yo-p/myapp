b1df73801feacf520760baca359fae8a7dd435db
小林　良輝 <papapapipipi42@gmail.com>
2020-07-18 10:29:45 +0900

    Add bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,15 +5,6 @@ const vue = require('./loaders/vue')
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 environment.config.resolve.alias = {'vue$': 'vue/dist/vue.esm.js' }
-// jQueryとBootstapのJSを使えるように
-const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: 'popper.js'
-  })
-)
+
 
 module.exports = environment
