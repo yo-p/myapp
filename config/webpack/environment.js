ad0411bfc2e24949bef54c699e4a5633f9d310b8
小林　良輝 <papapapipipi42@gmail.com>
2020-07-21 20:39:13 +0900

    Add Bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,19 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-environment.config.resolve.alias = {'vue$': 'vue/dist/vue.esm.js' }
+const webpack = require("webpack")
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+}))
+
+// const { VueLoaderPlugin } = require('vue-loader')
+// const vue = require('./loaders/vue')
+
+// environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+// environment.loaders.prepend('vue', vue)
+// environment.config.resolve.alias = {'vue$': 'vue/dist/vue.esm.js' }
 
 
 module.exports = environment
