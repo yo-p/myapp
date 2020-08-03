343d5ffe28831e7f31aacc64c72b62470a3b0edf
小林　良輝 <papapapipipi42@gmail.com>
2020-08-03 13:21:24 +0900

    Add Validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_061423) do
+ActiveRecord::Schema.define(version: 2020_08_03_034746) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
