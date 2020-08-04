2921af1b54628e6049f7e57250973de368b980aa
小林　良輝 <papapapipipi42@gmail.com>
2020-08-04 16:14:10 +0900

    Add Admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_055541) do
+ActiveRecord::Schema.define(version: 2020_08_04_061912) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", limit: 30, null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_08_04_055541) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
