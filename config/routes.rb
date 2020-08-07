68f4498b99b01c85523e2d1a5edd90d457d07629
小林　良輝 <papapapipipi42@gmail.com>
2020-08-07 16:15:49 +0900

    Add SessionController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'session#destroy'
+
   namespace :admin do
     resources :users
   end
